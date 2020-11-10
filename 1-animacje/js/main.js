2409840527f1ad6879b2066607a0f661219c4f3b
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-11-10 20:49:58 +0100

    animation to a button added

---
+++ 1-animacje/js/main.js
@@ -0,0 +1,22 @@
+/* 1. Stwórz przycisk - (element button)
+2. Pod przyciskiem stwórz kwadrat (znacznik div) o wysokości i
+szerokości 200px oraz kolorze czerwonym
+3. Po kliknięciu w przycisk kwadrat ma się przesunąć w prawo o 100px
+oraz ma się zmniejszyć do rozmiaru 100px na 100px - wykorzystaj do
+tego metodę animate() - animacja ma trwać 3 sekundy
+4. Po zakończonej animacji zmień kolor kwadratu na niebieski - animacja
+zmiany ma trwać 5 sekund
+5. Gdy kwadrat zmieni kolor na niebieski wyświetl wewnątrz niego napis
+w nagłówku h2 "Animacja zakończona" */
+
+$(document).ready(function() {
+    const square = $('.square');
+
+    $('#change').click(function() {
+        square.animate({'margin-left': '100px', 'height' : '100px', 'width': '100px'}, 3000, function() {
+            $(this).css('background-color','blue')
+
+        });
+        });
+
+    })
